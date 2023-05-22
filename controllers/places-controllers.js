975aa7030a7d8d94487ae858fcf9d61ed1532a8a
burakdiker <burak.diker@ug.bilkent.edu.tr>
2023-05-22 22:20:02 +0300

    places-controllers code arrangement

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -60,7 +60,7 @@ const createPlace = async (req, res, next) => {
     return next(new HttpError('Invalid input. Please check your data.', 422));
   }
 
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
 
   let coordinates;
 
@@ -75,13 +75,13 @@ const createPlace = async (req, res, next) => {
     description,
     location: coordinates,
     image: req.file.path,
-    creator,
+    creator: req.userData.userId,
   });
 
   let user;
 
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch (error) {
     return next(error);
   }
